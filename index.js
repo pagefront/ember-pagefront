a6112cb9eb0483e5fb25613227c4040e8642d5cb
Jason Kriss <jasonkriss@gmail.com>
2015-07-03 11:16:30 -0700

    add index adapter

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 /* jshint node: true */
 'use strict';
 
+var IndexAdapter = require('./lib/index-adapter');
 var AssetsAdapter = require('./lib/assets-adapter');
 
 module.exports = {
   name: 'ember-pagefront',
   type: 'ember-deploy-addon',
   adapters: {
+    index: {
+      'pagefront-index': IndexAdapter
+    },
     assets: {
       'pagefront-assets': AssetsAdapter
     }
