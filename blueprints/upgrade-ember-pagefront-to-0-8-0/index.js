68aab4a89f92fe919d16ccfb2c5f1a985cc23a9d
Jason Kriss <jasonkriss@gmail.com>
2015-11-05 17:11:48 -0800

    add upgrade blueprint

---
+++ blueprints/upgrade-ember-pagefront-to-0-8-0/index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var chalk = require('chalk');
+
+var green = chalk.green;
+
+var PAGEFRONT_CONFIG = '.pagefrontrc';
+var GITIGNORE = '.gitignore';
+var NEW_LINE = '\n';
+var DOT_ENV_FILE = '.env.deploy.*';
+var MESSAGE = NEW_LINE + green('Success!') + NEW_LINE;
+
+module.exports = {
+  description: 'upgrades configuration to match what ember-pagefront@0.8.0 expects',
+
+  locals: function() {
+    var key = '';
+
+    try {
+      var rawPagefrontrc = fs.readFileSync(PAGEFRONT_CONFIG);
+      key = JSON.parse(rawPagefrontrc).key;
+    } catch (e) {}
+
+    return { key: key };
+  },
+
+  afterInstall: function() {
+    try { fs.unlinkSync(PAGEFRONT_CONFIG); } catch (e) {}
+    fs.appendFileSync(GITIGNORE, NEW_LINE + DOT_ENV_FILE);
+    this.ui.writeLine(MESSAGE);
+  }
+};
