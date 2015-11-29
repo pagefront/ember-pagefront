d864dc4195267f7bbb3a50a4175a1f30f841ae70
Jason Kriss <jasonkriss@gmail.com>
2015-11-29 11:33:31 -0800

    add login command

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var commands = require('./lib/commands');
+
 module.exports = {
-  name: 'ember-pagefront'
+  name: 'ember-pagefront',
+
+  includedCommands: function() {
+    return commands;
+  }
 };
