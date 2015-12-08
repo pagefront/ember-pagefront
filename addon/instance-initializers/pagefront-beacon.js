3fd8f1bac9b01502f05590deee265611569f8e34
Jason Kriss <jasonkriss@gmail.com>
2015-12-07 17:59:55 -0800

    make instance initializer work for all versions of Ember

--- addon/instance-initializers/pagefront-beacon.js
+++ addon/instance-initializers/pagefront-beacon.js
@@ -3,21 +3,33 @@ import Ember from 'ember';
 const FUNCTION = 'function';
 const ACTION = 'pagefrontDidRelease';
 
+function isFunction(object) {
+  Ember.typeOf(object) === FUNCTION;
+}
+
+function lookup(applicationInstance, name) {
+  if (isFunction(applicationInstance.lookup)) {
+    return applicationInstance.lookup(name);
+  } else {
+    return applicationInstance.container.lookup(name);
+  }
+}
+
 function hasAction(route) {
   const actions = route.actions || route._actions;
 
-  return actions && Ember.typeOf(actions[ACTION]) === FUNCTION;
+  return actions && isFunction(actions[ACTION]);
 }
 
 function isEnabled(applicationInstance) {
-  const route = applicationInstance.lookup('route:application');
+  const route = lookup(applicationInstance, 'route:application');
 
   return route && hasAction(route);
 }
 
 export function initialize(applicationInstance) {
   if (isEnabled(applicationInstance)) {
-    applicationInstance.lookup('service:pagefront-beacon');
+    lookup(applicationInstance, 'service:pagefront-beacon');
   }
 }
 
